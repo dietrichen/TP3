d289eb748b9f4c526050bdc3c8c48c2dd76bea24
dietrichen <ed819118@wcupa.edu>
2016-12-14 17:02:20 -0500

    Bringing in James Scene Try 2
    Last time crashed everything had to download from github

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+	[SerializeField] private GameObject sceneCamera;
+	[SerializeField] private GameObject reticuleCanvas;
+	[SerializeField] private GameObject preGameCanvas;
+
+	public override void OnStartClient (NetworkClient client)
+	{
+		HideSceneCamera ();
+		ShowReticuleCanvas ();
+
+	}
+
+	public override void OnStartHost ()
+	{
+		HideSceneCamera ();
+		ShowReticuleCanvas ();
+	}
+
+	public override void OnStopClient ()
+	{
+		ShowSceneCamera ();
+		HideReticuleCanvas ();
+	}
+
+	public override void OnStopHost ()
+	{
+		ShowSceneCamera ();
+		HideReticuleCanvas ();
+	}
+
+	private void HideSceneCamera ()
+	{       
+		if (sceneCamera) {
+			sceneCamera.SetActive (false);
+			preGameCanvas.SetActive (false);
+		}
+	}
+
+	private void ShowSceneCamera ()
+	{       
+		if (sceneCamera) {
+			sceneCamera.SetActive (true);
+			preGameCanvas.SetActive (true);
+		}
+	}
+
+	private void HideReticuleCanvas ()
+	{
+		if (reticuleCanvas)
+			reticuleCanvas.SetActive (false);
+	}
+
+	private void ShowReticuleCanvas ()
+	{
+		if (reticuleCanvas)
+			reticuleCanvas.SetActive (true);
+	}
+}
